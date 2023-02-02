ec4bc01118f0495b0e679b56eb09d3cfc5f0b37b
shafik720 <shafikrasel5@gmail.com>
2023-02-02 11:53:14 +0600

    working on pagination

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -91,6 +91,7 @@ const Body = () => {
             }
             {pageSize}
             <select onChange={e=>setPageSize(e.target.value)}>
+                <option value="5">5</option>
                 <option selected value="10">10</option>
                 <option value="15">15</option>
                 <option value="20">20</option>
