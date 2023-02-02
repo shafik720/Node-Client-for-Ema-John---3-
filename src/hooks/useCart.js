5262b8e891fd53700ef93afd68c01b22c184c88f
shafik720 <shafikrasel5@gmail.com>
2023-02-02 13:16:15 +0600

    working on pagination

--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -2,23 +2,37 @@ import { useEffect, useState } from "react";
 import { getStoredCart } from "../utilities/fakedb";
 
 
-function useCart(products){
+function useCart() {
     let [cart, setCart] = useState([]);
 
-    useEffect(()=>{
-        const  storedCart = getStoredCart();
+    useEffect(() => {
+        const storedCart = getStoredCart();
         let freshCart = [];
-        for(let productId in storedCart){
-            let addedProduct = products.find(product=> product._id === productId);
-            
-            if(addedProduct){
-                let quantity = storedCart[productId];
-                addedProduct.quantity = quantity;
-                freshCart.push(addedProduct);
-            }
-        }        
-    setCart(freshCart);
-    },[products])
+        const keys = Object.keys(storedCart);
+
+        fetch('http://localhost:5000/productByKeys', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/JSON'
+            },
+            body: JSON.stringify(keys)
+        })
+            .then(res => res.json())
+            .then(products => {
+                for (let productId in storedCart) {
+                    let addedProduct = products.find(product => product._id === productId);
+
+                    if (addedProduct) {
+                        let quantity = storedCart[productId];
+                        addedProduct.quantity = quantity;
+                        freshCart.push(addedProduct);
+                    }
+                }
+                setCart(freshCart);
+            })
+
+
+    }, [])
     return [cart, setCart]
 }
 
